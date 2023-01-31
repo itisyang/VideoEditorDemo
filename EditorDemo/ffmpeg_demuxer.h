7c85b0b442ad183dfe57a5e6bec72be69dbc10ab
itisyang <itisyang@gmail.com>
2023-01-31 16:41:58 +0800

    fix:移除ffmpeg转nv格式函数

--- EditorDemo/ffmpeg_demuxer.h
+++ EditorDemo/ffmpeg_demuxer.h
@@ -338,21 +338,5 @@ public:
     }
 };
 
-inline cudaVideoCodec FFmpeg2NvCodecId(AVCodecID id) {
-    switch (id) {
-    case AV_CODEC_ID_MPEG1VIDEO: return cudaVideoCodec_MPEG1;
-    case AV_CODEC_ID_MPEG2VIDEO: return cudaVideoCodec_MPEG2;
-    case AV_CODEC_ID_MPEG4: return cudaVideoCodec_MPEG4;
-    case AV_CODEC_ID_WMV3:
-    case AV_CODEC_ID_VC1: return cudaVideoCodec_VC1;
-    case AV_CODEC_ID_H264: return cudaVideoCodec_H264;
-    case AV_CODEC_ID_HEVC: return cudaVideoCodec_HEVC;
-    case AV_CODEC_ID_VP8: return cudaVideoCodec_VP8;
-    case AV_CODEC_ID_VP9: return cudaVideoCodec_VP9;
-    case AV_CODEC_ID_MJPEG: return cudaVideoCodec_JPEG;
-    case AV_CODEC_ID_AV1: return cudaVideoCodec_AV1;
-    default: return cudaVideoCodec_NumCodecs;
-    }
-}
 
 
